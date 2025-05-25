738f965186c3d73328dfaf362e2cee68baa02e1f
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-05-25 23:09:48 +0800

    Update final_scanner_polygon_REBUILT_FULL_FIXED_KEYMATCHED.py

--- final_scanner_polygon_REBUILT_FULL_FIXED_KEYMATCHED.py
+++ final_scanner_polygon_REBUILT_FULL_FIXED_KEYMATCHED.py
@@ -8,7 +8,7 @@ import requests
 import pandas as pd
 import threading
 from datetime import datetime
-om polygon.rest import RESTClient
+from polygon.rest import RESTClient
 from ta.momentum import RSIIndicator, StochasticOscillator
 from ta.trend import MACD
 from google.oauth2.service_account import Credentials
