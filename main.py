d6ce75a416a75d1dfa63aba14892c2a5d999d674
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-05-27 21:52:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ def fetch_stock_data(symbol):
             ticker=symbol,
             multiplier=5,
             timespan="minute",
-            from_=start.isoformat(),
-            to=end.isoformat(),
+            from_=start.isoformat(timespec="seconds"),
+            to=end.isoformat(timespec="seconds"),
             limit=100,
             adjusted=True,
         )
