d473797bfa2c50d79ad46d128820afe6372619a7
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-05-27 15:33:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ SCAN_INTERVAL = 60
 def fetch_stock_data(symbol):
     try:
         client = RESTClient(api_key=API_KEY)
-        end = datetime.now()
+        end = datetime(2025, 5, 22, 15, 30)  # EST 時間 15:30
         start = end - timedelta(minutes=35)
 
         aggs = client.get_aggs(
