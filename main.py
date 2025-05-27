01ab15cc260abd1670fd8d404b7d2546460e5176
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-05-27 18:07:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import csv
 from datetime import datetime, timedelta
 import pandas as pd
 from pytz import timezone
-from polygon import RESTClient
+from polygon_api_client.client import RESTClient
 from ta.momentum import RSIIndicator, StochasticOscillator
 from ta.trend import MACD
 
