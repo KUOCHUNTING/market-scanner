aa755acf735ca10de078df4803eb5306eecd4635
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-05-28 23:43:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -203,7 +203,10 @@ def fetch_stock_data(symbol):
                 "extended_signal": extended_signal
                 
             }
-            
+     except Exception as e:
+        print(f"[ERROR] 抓取資料失敗 {symbol}：{e}")
+        return None
+         
     print(f"\n[統計] 本輪成功 {success_count} 檔，失敗 {fail_count} 檔，有效率：{round(success_count / (success_count + fail_count + 1e-6) * 100, 2)}%\n")
 
 if __name__ == "__main__":
