d91eee5da500160d6ea61f512da4537368a064fe
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-05-31 16:43:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ market_close = now_est.replace(hour=16, minute=0, second=0, microsecond=0)
 
 # 只在開盤期間運行
 if now_est < market_open or now_est > market_close:
-print("[INFO] 非美股盤中時間，跳過掃描")
-exit()
+    print("[INFO] 非美股盤中時間，跳過掃描")
+    exit()
 
 API_KEY = os.getenv("POLYGON_API_KEY") or "YmbcjRd1RA6l3pTlN0NvKRzd7OY4eV8k"
 STOCK_LIST_CSV = "filtered_us_stocks_common_only.csv"
