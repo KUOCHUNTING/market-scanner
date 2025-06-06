7cec7e76fed837e49146649cc67ff6baa45e633e
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-06-06 23:29:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -92,8 +92,11 @@ def init_sheets():
     except Exception as e:
         print(f"[ERROR] 初始化 Sheets 時失敗：{e}")   
 
-now_utc = datetime.now(dt_timezone.utc)
-now_est = datetime.now(timezone("US/Eastern"))
+from datetime import datetime, timedelta
+import pytz  # ✅
+
+now_utc = datetime.now(pytz.utc)
+now_est = datetime.now(pytz.timezone("US/Eastern"))
 # === 資料來源 ===
 DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1372956363235393536/2bELr_6LwGlk2K7G4B3d3J0MBD5iv04IwC33pQaWxAHcRbgn6sBVtkvI_65FfmC4Um5f"  # 記得換成自己的
 
