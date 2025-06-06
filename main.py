aed6ca92913b65d07dc0df542425cbe5df1ebd83
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-06-06 23:35:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -680,9 +680,9 @@ def detect_latent_signal(df, rsi, tmo, obv, latest_price, latest_vwap):
     return signal_note, auto_entry, direction
 
 
-from datetime import datetime, timezone
+from datetime import timezone
 # 設定美東時間
-now_utc = datetime.now(dt_timezone.utc)  # ✅ 使用標準庫 timezone.utc
+now_utc = datetime.now(timezone.utc)
 now_est = datetime.now(timezone("US/Eastern"))  # ✅ 使用 pytz 處理美東時間
 market_open = now_est.replace(hour=9, minute=30, second=0, microsecond=0)
 market_close = now_est.replace(hour=16, minute=0, second=0, microsecond=0)
