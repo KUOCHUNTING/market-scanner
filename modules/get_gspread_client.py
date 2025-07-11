8d674e834611d12e8e2067822c0e12fd2dccfa9d
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-11 18:39:39 +0800

    Delete modules/get_gspread_client.py

--- modules/get_gspread_client.py
+++
@@ -1,6 +0,0 @@
-def get_gspread_client(base64_key):
-
-    keyfile_dict = json.loads(base64.b64decode(base64_key))
-    scopes = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-    creds = Credentials.from_service_account_info(keyfile_dict, scopes=scopes)
-    return gspread.Client(auth=creds)
