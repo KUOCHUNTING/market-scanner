4d87a15a483116b23412dda748f1345cea50a5c9
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-11 18:38:29 +0800

    Delete modules/get_latest_price.py

--- modules/get_latest_price.py
+++
@@ -1,7 +0,0 @@
-def get_latest_price(symbol):
-    position = positions.get(symbol)
-    if position and "latest_price" in position:
-        return position["latest_price"]
-    else:
-        print(f"[⚠️ 無法取得價格] {symbol} 缺少 latest_price")
-        return None
