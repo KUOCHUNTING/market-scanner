509a94c6f7d53d57adada5cd6b119ae0638b64f2
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-16 00:47:51 +0800

    Update connect_to_gsheet.py

--- modules/utils/connect_to_gsheet.py
+++ modules/utils/connect_to_gsheet.py
@@ -4,9 +4,6 @@ import json
 import gspread
 from datetime import datetime
 from google.oauth2.service_account import Credentials
- # 讀取 .env 中的金鑰與網址
-key_base64 = os.getenv("GCP_KEY_BASE64")
-sheet_url = os.getenv("GSHEET_URL")
 
 
 # ✅ 取得憑證（從 base64 環境變數或參數）
