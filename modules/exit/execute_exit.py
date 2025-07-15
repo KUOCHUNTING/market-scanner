69702d2556f08a55d294d2c3a7ca0a7272b259e2
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-15 23:30:28 +0800

    Update execute_exit.py

--- modules/exit/execute_exit.py
+++ modules/exit/execute_exit.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from modules.utils.connect_to_gsheet import write_exit_to_sheet
+from modules.utils.connect_to_gsheet import connect_to_gsheet, write_exit_to_sheet
 from modules.notify.discord_push import send_discord_message
 from modules.config.config import WEBHOOK_URL
 from modules.utils.format import safe_float
