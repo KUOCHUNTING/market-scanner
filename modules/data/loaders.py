7c5558df8ea223bd627f1ac86eeeb0aed182a0d4
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-15 15:26:04 +0800

    Update loaders.py

--- modules/data/loaders.py
+++ modules/data/loaders.py
@@ -1,3 +1,11 @@
 from modules.utils.file_loader import load_stock_list
 from modules.fetch_stock_data import fetch_stock_data
 from modules.get_fundamentals import get_fundamentals
+
+# modules/data/loaders.py
+
+import pandas as pd
+
+def load_stock_list():
+    df = pd.read_csv("data/filtered_us_stocks_common_only.csv")
+    return df["symbol"].dropna().unique().tolist()
