66c8c8566d3fd445b145bbe9ef96ea903e70ba04
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-15 22:06:51 +0800

    Update check_exit_and_notify.py

--- modules/notify/check_exit_and_notify.py
+++ modules/notify/check_exit_and_notify.py
@@ -1,4 +1,3 @@
-from modules.config import DEFAULT_STOP_LOSS
 from datetime import datetime, timedelta
 from modules.notify.discord_push import send_discord_message
 from modules.exit.execute_exit import execute_exit
