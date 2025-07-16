bde7962d7c04466d72dd9730134d24a5b175bbd1
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-16 23:05:25 +0800

    Update enter_position.py

--- modules/entry/enter_position.py
+++ modules/entry/enter_position.py
@@ -58,7 +58,7 @@ def enter_position(
     position = {
         "symbol": symbol,
         "entry_time": entry_time,
-        "entry_price": entry_price,
+        "entry_price": price,
         "direction": direction,
         "quantity": quantity,
         "capital_used": capital_used,
