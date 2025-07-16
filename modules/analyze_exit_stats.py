314f10daeac70e4337b57952d61226520a17d8f6
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-16 19:57:13 +0800

    Update analyze_exit_stats.py

--- modules/analyze_exit_stats.py
+++ modules/analyze_exit_stats.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pandas as pd
 from datetime import datetime
 from modules.connect_to_gsheet import connect_to_gsheet
