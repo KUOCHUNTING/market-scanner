c9f8c012267950feebafe45f0df5c856db165253
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-16 19:58:16 +0800

    Update fetch_stock_data.py

--- modules/fetch_stock_data.py
+++ modules/fetch_stock_data.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pytz
 import pandas as pd
 from datetime import datetime, timedelta, time as dtime
