0026cda157b9acc0a7632a2f8b74c99975b593ab
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-18 23:58:25 +0800

    Update scan_market.py

--- modules/scan_market.py
+++ modules/scan_market.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import traceback
 import random
-
+from dotenv import load_dotenv
 from modules.utils.file_loader import load_stock_list
 from modules.fetch_stock_data import fetch_stock_data
 from modules.get_fundamentals import get_fundamentals
