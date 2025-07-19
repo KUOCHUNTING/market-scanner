b02d599d01e1427b0ea0dea879f9624f9483293f
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-19 22:21:23 +0800

    Update gsheet_writer.py

--- modules/utils/gsheet_writer.py
+++ modules/utils/gsheet_writer.py
@@ -7,7 +7,7 @@ import os
 from datetime import datetime
 import numpy as np
 import pandas as pd
-from modules.utils.connect_to_gsheet import connect_to_gsheet
+from modules.utils.gsheet_writer import write_exit_to_sheet
 
 # ✅ 安全轉換格式
 def to_serializable(value):
