2480a7078b8331279ccc959a6d1991a70275bcc9
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-20 20:17:03 +0800

    Delete test_webhook.py

--- test_webhook.py
+++
@@ -1,9 +0,0 @@
-import requests
-from modules.config import WEBHOOK_URL
-
-def test_discord_push():
-    msg = "✅ 測試推播成功！"
-    res = requests.post(WEBHOOK_URL, json={"content": msg})
-    print(res.status_code, res.text)
-
-test_discord_push()
