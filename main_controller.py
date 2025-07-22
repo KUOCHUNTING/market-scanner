988d082cdfae79e2b9b64f5698a5651016dd0f72
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-23 00:41:55 +0800

    Update main_controller.py

--- main_controller.py
+++ main_controller.py
@@ -18,9 +18,6 @@ pm = PositionManager(initial_capital=100000)
 # ✅ 載入 .env 環境變數
 load_dotenv()
 
-from modules.entry.enter_position import capital_left
-
-capital_left = 100000  # ✅ 主控程式明確初始化一次
 sheet_url = os.getenv("GSHEET_URL")
 key_base64 = os.getenv("GCP_KEY_BASE64")
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
