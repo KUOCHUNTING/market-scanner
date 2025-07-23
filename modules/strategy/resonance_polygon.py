e4cf644b800337950a1a472d933d336ee233c096
KUOCHUNTING <aian0968@yahoo.com.tw>
2025-07-23 23:04:47 +0800

    Update resonance_polygon.py

--- modules/strategy/resonance_polygon.py
+++ modules/strategy/resonance_polygon.py
@@ -37,12 +37,18 @@ def calculate_rsi(series, period=14):
     return 100 - (100 / (1 + rs))
 
 def calculate_obv(close, volume):
+    # ✅ 確保都是 Series，避免 .index 出錯
+    if not isinstance(close, pd.Series):
+        close = pd.Series(close)
+    if not isinstance(volume, pd.Series):
+        volume = pd.Series(volume)
+
     obv = [0]
     for i in range(1, len(close)):
-        if close[i] > close[i-1]:
-            obv.append(obv[-1] + volume[i])
-        elif close[i] < close[i-1]:
-            obv.append(obv[-1] - volume[i])
+        if close.iloc[i] > close.iloc[i - 1]:
+            obv.append(obv[-1] + volume.iloc[i])
+        elif close.iloc[i] < close.iloc[i - 1]:
+            obv.append(obv[-1] - volume.iloc[i])
         else:
             obv.append(obv[-1])
     return pd.Series(obv, index=close.index)
